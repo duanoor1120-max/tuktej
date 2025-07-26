a7f0ca3b9102741d6b5d7ca1983c99c1ded6c9fe
codewithsalman12 <salmanb799gamil.com@gmail.com>
2025-07-26 10:51:17 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+$("#hide").click(function () {
+  $(".card").hide();
+});
+$("#hideall").click(function () {
+  $("*").hide();
+});
+$("#show").click(function () {
+  $(".card").show();
+});
+$('#dialog').hide()
+$("#h").click(function () {
+    $("#dialog").dialog();
+  }
+);
